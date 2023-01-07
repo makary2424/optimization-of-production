75cfbf404d90e252a167b25ad15c503b60f48132
Makariy <Reddi24@yandex.ru>
2023-01-07 21:12:08 +0300

    добавил номер партии и все сломал

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ var f_types = ["Сельдь", "Скумбрия", "Форель", "Кета", "
 
 function addFish(){
     let tr = document.createElement('tr');
-    let td1 = document.createElement('td')
-    let td2 = document.createElement('td')
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
     td1.appendChild(document.createTextNode('Вид сырья:'));
     let val;  
     var $select = document.createElement('select');
-    for (val of f_types)
+    for (val of f_types);
     {
     $option = createOption(val);
     $select.appendChild($option);
@@ -27,9 +27,41 @@ function addFish(){
     td2.appendChild($select);
     tr.appendChild(td1);
     tr.appendChild(td2);
-    let last_fish = document.getElementsByClassName('fish_weight')[0]
+    let last_fish = document.getElementsByClassName('fish_type')[0];
     last_fish.parentNode.insertBefore(tr, last_fish.nextSibling);
     
 }
+function fishWeight(){
+    let tr2 = document.createElement("tr");
+    let td12 = document.createElement('td');
+    let td22 = document.createElement('td');
+    td12.appendChild(document.createTextNode("Вес:"));
+    td22.appendChild(document.createElement("input"));
+    tr2.appendChild(td12);
+    tr2.appendChild(td22);
+    let last_fish = document.getElementsByClassName('fish_weight')[0];
+    last_fish.parentNode.insertBefore(tr2, last_fish.nextSibling);
+}
 
-btn.addEventListener("click", addFish);
+function number(){
+    let tr3 = document.createElement("tr");
+    let td13 = document.createElement('td');
+    let td23 = document.createElement('td');
+    td13.appendChild(document.createTextNode("Вес:"));
+    td23.appendChild(document.createElement("input"));
+    tr3.appendChild(td13);
+    tr3.appendChild(td23);
+    let last_fish = document.getElementsByClassName('number')[0];
+    last_fish.parentNode.insertBefore(tr3, last_fish.nextSibling);
+
+
+}
+
+function addItem(){
+    number();
+    addFish();
+    fishWeight();
+    
+    
+}
+btn.addEventListener("click", addItem);
