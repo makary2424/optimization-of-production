23b6a20845add62a4b4c9ac32985da669e1e27c7
Makariy <Reddi24@yandex.ru>
2023-01-07 19:55:50 +0300

    добавили вид рыбы по нажатию на +

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 let btn = document.getElementById("add_fish");
+
+
+
+const createOption = value => {
+  const $option = document.createElement('option');
+  $option.textContent = value;
+  $option.value = value;
+  return $option;
+};
+
+
+var f_types = ["Сельдь", "Скумбрия", "Форель", "Кета", "Горбуша"];
+
 function addFish(){
-    let new_p = document.createElement('p');
-    let fishes = document.getElementById("fishes");
-    new_p.appendChild(document.createTextNode('Добавили рыбку'));
-    fishes.appendChild(new_p);
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td')
+    let td2 = document.createElement('td')
+    td1.appendChild(document.createTextNode('Вид сырья:'));
+    let val;  
+    var $select = document.createElement('select');
+    for (val of f_types)
+    {
+    $option = createOption(val);
+    $select.appendChild($option);
+    }
+    td2.appendChild($select);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    let last_fish = document.getElementsByClassName('fish_weight')[0]
+    last_fish.parentNode.insertBefore(tr, last_fish.nextSibling);
+    
 }
+
 btn.addEventListener("click", addFish);
